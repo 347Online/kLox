a09e7c67df85af29e096d75050fa9174577d3eef
347Online <38228372+347Online@users.noreply.github.com>
2023-03-11 16:15:39 -0600

    Main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+
+
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let len = args.len();
+
+    match len {
+        1 => Lox::run_prompt(),
+        2 => Lox::run_file(args[1]),
+        _ => println!(),
+    }
+
+    if len > 2 {
+        println!("Usage: klox-vm [script]")
+    } else if len == 2 {
+        // Lox::run_file(args[1]);
+    } else {
+        Lox::run_prompt();
+    }
 }
