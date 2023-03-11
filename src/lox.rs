02174df98c453c002a768593530b454a99256ef8
347Online <38228372+347Online@users.noreply.github.com>
2023-03-11 16:50:35 -0600

    Beginning of Prompt

--- src/lox.rs
+++ src/lox.rs
@@ -1,4 +1,4 @@
-use std::fs::read_to_string;
+use std::{fs::read_to_string, io::*};
 
 pub struct Lox {
     //
@@ -11,6 +11,19 @@ impl Lox {
     }
 
     pub fn run_prompt() {
+        let stdin = stdin();
+        let mut stdout = stdout();
+        loop {
+            print!("> ");
+            stdout.flush().unwrap();
 
+            let mut line = String::new();
+            stdin.read_line(&mut line).expect("Failed to read stdin");
+            Lox::run(line);
+        }
+    }
+
+    pub fn run(code: String) {
+        print!("{code}");
     }
 }
