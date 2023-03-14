9a9f5df0d78d1f54df33e36c2e7852d12ae79c79
347Online <38228372+347Online@users.noreply.github.com>
2023-03-14 18:27:59 -0500

    Change to use associated methods of stateless Lox

--- src/main.rs
+++ src/main.rs
@@ -14,11 +14,9 @@ fn main() -> Result<(), String> {
     let args: Vec<String> = env::args().collect();
     let len = args.len();
 
-    let mut lox = Lox::new();
-
     match len {
-        1 => lox.run_prompt().map_err(|e| e.to_string()),
-        2 => lox.run_file(args[1].clone()).map_err(|e| e.to_string()),
+        1 => Lox::run_prompt().map_err(|e| e.to_string()),
+        2 => Lox::run_file(args[1].clone()).map_err(|e| e.to_string()),
         _ => Err(String::from("Usage: klox [script]")),
     }
 }
