1c9c7dba1e3bb9799299dae55d6a42fc2e6fc925
347Online <38228372+347Online@users.noreply.github.com>
2023-03-19 10:11:06 -0500

    Add clock function

--- src/callable.rs
+++ src/callable.rs
@@ -1,4 +1,7 @@
-use std::fmt::{Debug, Display};
+use std::{
+    fmt::{Debug, Display},
+    time::{SystemTime, UNIX_EPOCH},
+};
 
 use crate::{error::LoxError, interpreter::Interpreter, value::Value};
 
@@ -20,12 +23,12 @@ impl Clone for Box<dyn Call> {
 
 #[derive(Debug, Clone)]
 pub struct Function {
-    arity: usize
+    arity: usize,
 }
 
 impl Call for Function {
     fn arity(&self) -> usize {
-        self.arity()
+        self.arity
     }
 
     fn call(
@@ -44,5 +47,39 @@ impl Call for Function {
 impl Display for Function {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(f, "")
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct Clock {
+    arity: usize,
+}
+
+impl Call for Clock {
+    fn call(
+        &mut self,
+        _interpreter: &mut Interpreter,
+        _arguments: Vec<Value>,
+    ) -> Result<Value, LoxError> {
+        let time = SystemTime::now()
+            .duration_since(UNIX_EPOCH)
+            .expect("Failed to get the system time")
+            .as_secs() as f64;
+
+        Ok(Value::Number(time))
+    }
+
+    fn arity(&self) -> usize {
+        self.arity
+    }
+
+    fn box_clone(&self) -> Box<dyn Call> {
+        Box::new(self.clone())
+    }
+}
+
+impl Display for Clock {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "<native fn>")
     }
 }
