9ee8cab0df8dea027de9aaeb13c1c3c5376cf213
347Online <38228372+347Online@users.noreply.github.com>
2023-04-13 00:31:44 -0500

    Initial implementation of a single Instruction
    An instruction contains its operands
    
    This is different from the book in which we operate on bytes directly

---
+++ bytecode/src/lox/instruction.rs
@@ -0,0 +1,43 @@
+#[derive(Debug)]
+#[repr(u8)]
+pub enum Instruction {
+    Return,
+    Constant(u8),
+}
+
+use Instruction::*;
+impl Instruction {
+    pub fn opcode(&self) -> u8 {
+        // SAFETY: Because `Self` is marked `repr(u8)`, its layout is a `repr(C)` `union`
+        // between `repr(C)` structs, each of which has the `u8` discriminant as its first
+        // field, so we can read the discriminant without offsetting the pointer.
+        //
+        // Copied verbatim from https://doc.rust-lang.org/std/mem/fn.discriminant.html#accessing-the-numeric-value-of-the-discriminant
+        unsafe { *<*const _>::from(self).cast::<u8>() }
+    }
+
+    pub fn as_bytes(&self) -> Vec<u8> {
+        let code = self.opcode();
+
+        match self {
+            Constant(index) => vec![code, *index],
+            Return => vec![code],
+        }
+    }
+
+    pub fn disassemble(&self) -> (String, usize) {
+        let len = self.as_bytes().len();
+
+        fn simple(name: &str) -> String {
+            name.to_string()
+        }
+
+        use Instruction::*;
+        let repr = match self {
+            Return => simple("Return"),
+            Constant(index) => format!("Constant, Index: {:04}", index),
+        };
+
+        (repr, len)
+    }
+}
