53cfe2661998933dc272def8711752dc65a69ad0
347Online <38228372+347Online@users.noreply.github.com>
2023-04-13 21:38:08 -0500

    Add value stack to VM

--- bytecode/src/lox/vm.rs
+++ bytecode/src/lox/vm.rs
@@ -1,19 +1,24 @@
 use crate::lox::instruction::Instruction;
 
-use super::{error::LoxError, chunk::Chunk};
+use super::{error::LoxError, chunk::Chunk, value::Value};
 
 type InterpretResult = Result<(), LoxError>;
 
+const STACK_MAX: usize = 256;
+
 pub struct VirtualMachine {
     chunk: Option<Chunk>,
-    ptr: usize,
+    stack: [Value; STACK_MAX],
+    stack_ptr: usize,
 }
 
 impl VirtualMachine {
+
     pub fn new() -> Self {
         VirtualMachine {
             chunk: None,
-            ptr: 0
+            stack: [0.0; STACK_MAX],
+            stack_ptr: 0,
         }
     }
     
@@ -24,16 +29,35 @@ impl VirtualMachine {
         self.run()
     }
 
+    pub fn push(&mut self, value: Value) {
+        self.stack[self.stack_ptr] = value;
+        self.stack_ptr += 1;
+    }
+
+    pub fn pop(&mut self) -> Value {
+        self.stack_ptr -= 1;
+        self.stack[self.stack_ptr]
+    }
+
     fn run(&mut self) -> InterpretResult {
         let chunk = self.chunk.take().unwrap();
+        
+        #[cfg(debug_assertions)]
+        println!("{}", chunk.disassemble());
+
         use Instruction::*;
         for instruction in chunk.instructions() {
+
             match instruction {
                 Constant(index) => {
                     let constant = chunk.read_constant(*index as usize);
-                    println!("{}", constant);
+                    self.push(constant);
                 },
-                Return => return Ok(()),
+                Return => {
+                    let value = self.pop();
+                    println!("{}", value);
+                    return Ok(())
+                },
             }
         }
 
