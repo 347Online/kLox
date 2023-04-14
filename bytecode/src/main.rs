2557eb9e4e32cfde05b8c756961c9f880b831322
347Online <38228372+347Online@users.noreply.github.com>
2023-04-13 22:58:35 -0500

    Cleanup Main

--- bytecode/src/main.rs
+++ bytecode/src/main.rs
@@ -1,30 +1,5 @@
-use lox::Lox;
-
-use crate::lox::{chunk::Chunk, instruction::Instruction, vm::VirtualMachine};
-
 pub mod lox;
-
-// fn main() {
-//     use Instruction::*;
-
-//     let mut chunk = Chunk::new("test chunk");
-
-//     let constant = chunk.add_constant(1.2);
-//     chunk.write(Constant(constant), 123);
-
-//     let constant = chunk.add_constant(3.4);
-//     chunk.write(Constant(constant), 123);
-//     chunk.write(Add, 123);
-
-//     let constant = chunk.add_constant(5.6);
-//     chunk.write(Constant(constant), 123);
-//     chunk.write(Divide, 123);
-//     chunk.write(Negate, 123);
-//     chunk.write(Return, 123);
-
-//     let mut vm = VirtualMachine::new();
-//     let _ = vm.interpret(chunk);
-// }
+use lox::Lox;
 
 fn main() {
     let args: Vec<String> = std::env::args().collect();
