d1068881dadc4e6e6170f395d8b9327dc1e5cffe
347Online <38228372+347Online@users.noreply.github.com>
2023-04-19 12:44:11 -0500

    Safety comment on unsafe block in Instruction::try_from::<u8>()

--- bytecode/src/repr/opcode.rs
+++ bytecode/src/repr/opcode.rs
@@ -15,6 +15,10 @@ impl TryFrom<u8> for Instruction {
 
     fn try_from(value: u8) -> Result<Self, Self::Error> {
         if (0..=MAX_OPCODE).contains(&value) {
+            // SAFETY:
+            // MAX_OPCODE is derived from Instruction::Return, the final variant
+            // Since Instruction is defined as repr(u8), the variants form a contiguous range
+            // any u8 value less than or equal to Instruction::Return as u8 is a valid instruction
             let instruction = unsafe { std::mem::transmute(value) };
             Ok(instruction)
         } else {
