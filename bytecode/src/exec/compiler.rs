268034c3e41fd0a5f2d90f11741e85f4a0c0a584
347Online <38228372+347Online@users.noreply.github.com>
2023-04-29 04:00:35 -0500

    For loops! Per Bob, the implementation is now turing-complete

--- bytecode/src/exec/compiler.rs
+++ bytecode/src/exec/compiler.rs
@@ -177,6 +177,8 @@ impl Compiler {
     fn statement(&mut self) {
         if self.catch(TokenType::Print) {
             self.print();
+        } else if self.catch(TokenType::For) {
+            self.for_statement();
         } else if self.catch(TokenType::If) {
             self.if_statement();
         } else if self.catch(TokenType::While) {
@@ -224,6 +226,53 @@ impl Compiler {
 
         self.patch_jump(exit_jump);
         self.emit(Instruction::Pop);
+    }
+
+    fn for_statement(&mut self) {
+        self.begin_scope();
+        self.consume(TokenType::LeftParen, "Expect '(' after 'for'.");
+        // self.consume(TokenType::Semicolon, "Expect ';'.");
+        if self.catch(TokenType::Semicolon) {
+            // No initializer
+        } else if self.catch(TokenType::Var) {
+            self.var_declaration();
+        } else {
+            self.expression_statement();
+        }
+
+        let mut loop_start = self.chunk.len();
+        let exit_jump: Option<usize>;
+        if !self.catch(TokenType::Semicolon) {
+            self.expression();
+            self.consume(TokenType::Semicolon, "Expect ';' after loop condition.");
+
+            exit_jump = Some(self.emit_jump(Instruction::JumpIfFalse));
+            self.emit(Instruction::Pop);
+        } else {
+            exit_jump = None;
+        }
+
+        if !self.catch(TokenType::RightParen) {
+            let body_jump = self.emit_jump(Instruction::Jump);
+            let increment_start = self.chunk.len();
+            self.expression();
+            self.emit(Instruction::Pop);
+            self.consume(TokenType::RightParen, "Expect ')' after for clauses.");
+
+            self.emit_loop(loop_start);
+            loop_start = increment_start;
+            self.patch_jump(body_jump);
+        }
+
+        self.statement();
+        self.emit_loop(loop_start);
+
+        if let Some(exit_jump) = exit_jump {
+            self.patch_jump(exit_jump);
+            self.emit(Instruction::Pop);
+        }
+
+        self.end_scope();
     }
 
     fn begin_scope(&mut self) {
